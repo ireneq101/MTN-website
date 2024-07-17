a3dab306148686901744bdf354fd7bd1876127a0
ireneq101 <ireneqomoregbee@gmail.com>
2024-07-17 15:57:30 +0100

    done

---
+++ nav.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('header > .irene > div > .open')
+const close = document.querySelector('header > .irene > div > .close')
+const nav2 = document.querySelector('.nav2')
+
+hamburger.addEventListener('click', ()=> {
+    hamburger.classList.add('hide')
+    close.classList.add('hide')
+    nav2.classList.toggle('visible')
+})
+
+close.addEventListener('click', ()=>{
+    hamburger.classList.remove('hide')
+    close.classList.remove('hide')
+    nav2.classList.toggle('visible')
+})
+
+{/* <script> */}
+AOS.init();
+{/* </script> */}
